95ffca0cbf702f95fc9e990ff7e61cd4caa86b23
Rajasree Rajendran <me.shree@gmail.com>
2018-11-02 23:21:44 -0700

    Add files via upload

---
+++ CToF.py
@@ -0,0 +1,8 @@
+# PROGRAM to convert c to f
+def temp(degree_celsius: float):
+    # degreeCelsius = float(input("Enter temp in c: "))
+    farenHeit = 1.8 * degree_celsius - 32
+    return farenHeit
+
+
+print("Temp in f: ", temp(input('Enter temperature in degree celsius:')))
